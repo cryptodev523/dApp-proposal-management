8a875987fa913da063ff72c7e03cf714617a7702
cryptodev523 <jonejordan523@gmail.com>
2023-11-14 16:36:18 -0600

    integrate vote endpoint

--- frontend/src/components/ProposalList.tsx
+++ frontend/src/components/ProposalList.tsx
@@ -7,11 +7,44 @@ import {
   ListItem,
   Text,
   VStack,
+  useToast,
 } from "@chakra-ui/react";
+import axios from "axios";
 import { FaThumbsUp, FaThumbsDown } from "react-icons/fa";
 
 export const ProposalList = ({ proposals }: { proposals: IProposal[] }) => {
-  const handleVote = (id: string, vote: boolean) => {};
+  const toast = useToast();
+
+  const handleVote = async (id: string, vote: boolean) => {
+    try {
+      const response = await axios.post(
+        `${process.env.REACT_APP_BE_SERVER}/proposals/${id}/vote`,
+        { isYesVote: vote }
+      );
+
+      if (response.status === 200) {
+        toast({
+          title: "Vote submitted.",
+          description: "Your vote has been submitted.",
+          status: "success",
+          duration: 5000,
+          isClosable: true,
+        });
+      } else {
+        throw new Error("Failed to submit vote");
+      }
+    } catch (error) {
+      console.error("Failed to vote", error);
+
+      toast({
+        title: "Failed to submit vote.",
+        description: "An error occurred while submitting your vote.",
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+      });
+    }
+  };
 
   return (
     <Box my={12}>
@@ -37,7 +70,7 @@ export const ProposalList = ({ proposals }: { proposals: IProposal[] }) => {
                   Yes Votes: {proposal.yesVotes}
                 </Button>
                 <Button
-                  onClick={() => handleVote(proposal.id, true)}
+                  onClick={() => handleVote(proposal.id, false)}
                   leftIcon={<Icon as={FaThumbsDown} />}
                 >
                   No Votes: {proposal.noVotes}
